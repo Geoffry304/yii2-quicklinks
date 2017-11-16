df303738460f17ca7933c9acd43c8c86feac21e2
Geoffry Van den Eede <G.Vandeneede@dvit.be>
2017-11-16 15:55:36 +0100

    change package for icon

--- views/quicklink/_form.php
+++ views/quicklink/_form.php
@@ -1,7 +1,7 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use sjaakp\symbolpicker\SymbolPicker;
+use geoffry304\symbolpicker\SymbolPicker;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Quicklink */
@@ -17,14 +17,14 @@ use sjaakp\symbolpicker\SymbolPicker;
     <?= $form->field($model, 'link')->textInput(['placeholder' => \yii\helpers\Url::base(true) ]) ?>
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-    
+
     <?= $form->field($model, 'icon')->widget(SymbolPicker::className()) ?>
 
     <?= $form->field($model, 'newwindow')->dropDownList(geoffry304\quicklinks\models\Quicklink::valuesNewwindow()) ?>
 
     <?= $form->field($model, 'position')->textInput(['type' => "number", 'min' => "0"]) ?>
 
-  
+
 	<?php if (!Yii::$app->request->isAjax){ ?>
 	  	<div class="form-group">
 	        <?= Html::submitButton($model->isNewRecord ? Yii::t('quicklink', 'Create') : Yii::t('quicklink', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
@@ -32,5 +32,5 @@ use sjaakp\symbolpicker\SymbolPicker;
 	<?php } ?>
 
     <?php ActiveForm::end(); ?>
-    
+
 </div>
